1eb6f0dfa4677c88f85085026c0112993f1594c0
ServChan <divannikola.10@gmail.com>
2025-01-20 00:19:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,100 @@
-import discord, os, asyncio, time
+import discord
+import os
 from discord.ext import commands
-from trash import *
+from trash import logging
 
-bot = commands.Bot(command_prefix='>')
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+class ChannelCache:
+    """Класс для управления кэшем каналов."""
+    def __init__(self):
+        self.cache = {}
+
+    def load_cache(self, guilds):
+        self.cache = {
+            channel.id: channel.name
+            for guild in guilds
+            for channel in guild.channels
+        }
+        print("Кэш каналов загружен")
+
+    def get_channel_name(self, channel_id):
+        return self.cache.get(channel_id, "Канал не найден")
+
+channel_cache = ChannelCache()
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
+    channel_cache.load_cache(bot.guilds)
+    print(f'Logged in as {bot.user.name}')
+
+async def log_to_file(filename, message):
+    """Асинхронное логирование в файл."""
+    async with aiofiles.open(filename, mode='a', encoding='utf-8') as file:
+        await file.write(message + '\n')
 
 @bot.event
 async def on_message_delete(message):
-    if message.author.bot == False:
-        m = str(message.content)
-        n = str(message.author.name)
-        mes = n + " удалил сообщение ```" + m + "```"
+    if not message.author.bot:
+        mes = f"{message.author.name} удалил сообщение: ```{message.content}```"
         await message.channel.send(mes)
         logging("Deleted", mes)
-    else:
-        pass
+        await log_to_file("deleted_messages.log", mes)
 
 @bot.event
 async def on_message_edit(before, after):
-    if (after.author.bot == False) and not (before == after):
-        m = str(before.content)
-        n = str(before.author.name)
-        ma = str(after.content)
-        mes = n + " изменил сообщение. ```Ранее - " + m + "\nТеперь - " + ma + "```"
+    if not after.author.bot and before.content != after.content:
+        mes = (
+            f"{before.author.name} изменил сообщение.\n"
+            f"```Ранее - {before.content}\nТеперь - {after.content}```"
+        )
         await before.channel.send(mes)
         logging("Edit", mes)
-    else:
-        pass
+        await log_to_file("edited_messages.log", mes)
 
 @bot.event
-async def on_channel_create(chann):
-    name = chann.name
-    await chann.channel.send("Что это тут у нас? Новый канал?")
-    logging("NEW", "Channel created - " + name)
+async def on_channel_create(channel):
+    if isinstance(channel, discord.TextChannel):
+        mes = "Что это тут у нас? Новый канал?"
+        await channel.send(mes)
+        logging("NEW", f"Channel created - {channel.name}")
 
 @bot.event
 async def on_channel_update(before, after):
-    istherebe = 0
-    channafter = before.name
-    topicafter = before.topic
-    channnow = after.name
-    topicnow = after.topic
-    if channafter != channnow:
-        m = "А у нас тут ремонт! ```[НАЗВАНИЕ]\n    Ранее - " + channafter + "\n    Теперь - " + channnow + "```"
-        await after.channel.send(m)
-        istherebe = 1
-    if topicafter != topicnow:
-        if istherebe == 0:
-            m = "А у нас тут ремонт! ```[ОПИСАНИЕ]\n    Ранее - " + topicafter + "\n    Теперь - " + topicnow + "```"
-        elif istherebe == 1:
-            m = "```[ОПИСАНИЕ]\n    Ранее - " + topicafter + "\n    Теперь - " + topicnow + "```"
-        await after.channel.send(m)
-    logging("EDIT", "Channel edited - " + channnow)
+    changes = []
+
+    if before.name != after.name:
+        changes.append(f"[НАЗВАНИЕ]\n    Ранее - {before.name}\n    Теперь - {after.name}")
+
+    if before.topic != after.topic:
+        changes.append(f"[ОПИСАНИЕ]\n    Ранее - {before.topic or 'Нет описания'}\n    Теперь - {after.topic or 'Нет описания'}")
+
+    if changes:
+        mes = "А у нас тут ремонт!\n```" + "\n".join(changes) + "```"
+        if isinstance(after, discord.TextChannel):
+            await after.send(mes)
+        logging("EDIT", f"Channel edited - {after.name}")
+
+@bot.command()
+async def help(ctx):
+    help_message = (
+        "Доступные команды:\n"
+        ">hello - Поприветствовать бота.\n"
+        ">help - Показать это сообщение помощи.\n"
+        ">get_channel_name <id> - Получить название канала по его ID."
+    )
+    await ctx.send(f"```{help_message}```")
+    logging('Command', '>help')
+
+@bot.command()
+async def get_channel_name(ctx, channel_id: int):
+    channel_name = channel_cache.get_channel_name(channel_id)
+    await ctx.send(f"Название канала: {channel_name}")
+    logging('Command', f'>get_channel_name {channel_id}')
 
 @bot.command()
 async def hello(ctx):
